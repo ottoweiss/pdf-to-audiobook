714f5f657096bde3c147208fe56a08b152098169
ottocaroweiss <94467256+ottocaroweiss@users.noreply.github.com>
2024-09-13 12:48:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.pdf_to_json import extract_text_from_pdf
 import os
 import time
 from colorama import Fore, Style
-import inquirer
+import inquirer 
 import sys
 
 def input_q(text):
@@ -52,15 +52,22 @@ if __name__ == "__main__":
             page_start = int(input_q("What page should the audiobook start?: ").strip())
             page_end = int(input_q("What page should the audiobook end?: ").strip())
             title = input_q("Enter Your Book Title: ").strip()
-            cost, e_time = extract_text_from_pdf(pdf_file_name, {title: (page_start, page_end)})
+            n_cost, e_time, h_cost = extract_text_from_pdf(pdf_file_name, {title: (page_start, page_end)})
             correct_page_range = True
         except ValueError:
             print_error("Please provide a valid integer for page numbers.")
         except Exception as e:
             print_error(f"Error while converting PDF: {e}")
-
-    print_info(f"ESTIMATED COST FOR CONVERSION: {cost}")
-    print_info(f"ESTIMATED TIME FOR CONVERSION: {e_time}")
+    
+    print_info(f"ESTIMATED TIME FOR CONVERSION: {e_time}\n")
+    audio_model = ""
+    while  audio_model not in ["tts-1-hd", "tts-1"]:
+        audio_quality = input_q(f"Would you like normal (cost ~${n_cost}) or high (cost ~${h_cost} audio quality? [n]/h: ").strip()
+        if audio_quality == "h":
+            audio_model = "tts-1-hd"
+        elif audio_quality == "n" or audio_quality == "":
+            audio_model = "tts-1"
+    
     print_info("CLEANING TEXT with GPT...\n")
     json_file = pdf_file_name.replace(".pdf", ".json")
     output_json = json_file.replace(".json", "-cleaned.json")
@@ -84,7 +91,7 @@ if __name__ == "__main__":
             speed = float(speed)
             if 0.1 <= speed <= 3.0:
                 print_info("CONVERTING Text to Audiobook with GPT...\n")
-                get_audiobook(output_json, title, voice, speed)
+                get_audiobook(output_json, title, voice, speed, audio_model)
                 correct_audio_configuration = True
                 print_success(f"COMPLETE - Audiobook mp3 found in folder: {title}")
             else:
