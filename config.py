f78f2ab437c01bd6a6999f5d14042b20edcc6dc8
ottocaroweiss <94467256+ottocaroweiss@users.noreply.github.com>
2024-09-13 12:20:38 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
 KEY = ""
+
+TEXT_CLEANING_SYSTEM_PROMPT = "You are an AI that rewrites text extracted from pdfs of books. You write exactly what the book has in the main body of text, but you remove all typos, footnotes, headers, and page numbers. You always respond with the exact cleaned text of the main body of the book excerpt."
+TEXT_CLEANING_PROMPT = "Please clean the following text, removing footnotes, typos, headers (ex. \'\nChapter 1 \n29\'), page numbers, and unneccesary 'newline' characters. Respond with the exact cleaned text: \n"
+
+# Use get_rewrite in src/clean_pdf.py with the two json files to test cleaning prompt (maybe cut to only a few in the non-cleaned text for testing)
